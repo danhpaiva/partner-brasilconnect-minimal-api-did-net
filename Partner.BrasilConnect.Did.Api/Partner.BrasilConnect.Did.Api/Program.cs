f80aa53a70e07fac4a46f90a981882f053cdad19
Paiva <danpaiva@live.com>
2025-11-15 11:09:36 -0300

    feat: add documentacao para a api usando a lib Scalar

--- Partner.BrasilConnect.Did.Api/Partner.BrasilConnect.Did.Api/Program.cs
+++ Partner.BrasilConnect.Did.Api/Partner.BrasilConnect.Did.Api/Program.cs
@@ -2,9 +2,17 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Partner.BrasilConnect.Did.Api.Data;
 using Partner.BrasilConnect.Did.Api.Endpoints;
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("AppDbContext") ?? throw new InvalidOperationException("Connection string 'AppDbContext' not found.")));
+
+builder
+    .Services
+    .AddDbContext<AppDbContext>(options =>
+    options
+    .UseSqlite(builder
+    .Configuration
+    .GetConnectionString("AppDbContext") ?? throw new InvalidOperationException("Connection string 'AppDbContext' not found.")));
 
 builder.Services.AddOpenApi();
 
@@ -13,6 +21,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
