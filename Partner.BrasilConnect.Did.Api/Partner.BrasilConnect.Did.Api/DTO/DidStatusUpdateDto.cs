02ead8e78ea323463cc704d1ed9376159397f07c
Paiva <danpaiva@live.com>
2025-11-15 11:20:41 -0300

    refact: adicao de classes dtos

---
+++ Partner.BrasilConnect.Did.Api/Partner.BrasilConnect.Did.Api/DTO/DidStatusUpdateDto.cs
@@ -0,0 +1,11 @@
+﻿using Partner.BrasilConnect.Did.Api.Enum;
+using System.ComponentModel.DataAnnotations;
+
+namespace Partner.BrasilConnect.Did.Api.DTO;
+
+public record DidStatusUpdateDto
+{
+    [Required(ErrorMessage = "Campo Status obrigatorio.")]
+    public DidStatus Status { get; init; }
+    public string? ErrorMessage { get; init; }
+}
