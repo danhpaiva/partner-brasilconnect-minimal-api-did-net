810582a169d105001c311a1af77b1a024f50c166
Paiva <danpaiva@live.com>
2025-11-15 11:22:09 -0300

    feat: add metodo para ativar did e para atualizar status

--- Partner.BrasilConnect.Did.Api/Partner.BrasilConnect.Did.Api/Endpoints/DidActivationEndpoints.cs
+++ Partner.BrasilConnect.Did.Api/Partner.BrasilConnect.Did.Api/Endpoints/DidActivationEndpoints.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Http.HttpResults;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
 using Partner.BrasilConnect.Did.Api.Data;
+using Partner.BrasilConnect.Did.Api.DTO;
+using Partner.BrasilConnect.Did.Api.Enum;
 using Partner.BrasilConnect.Did.Api.Models;
 namespace Partner.BrasilConnect.Did.Api.Endpoints;
 
@@ -9,6 +11,44 @@ public static class DidActivationEndpoints
     public static void MapDidActivationEndpoints (this IEndpointRouteBuilder routes)
     {
         var group = routes.MapGroup("/api/DidActivation").WithTags(nameof(DidActivation));
+
+        group.MapPost("/request", async (DidCreationDto didCreationDto, AppDbContext db) =>
+        {
+            var didActivation = new DidActivation
+            {
+                DidNumber = didCreationDto.DidNumber,
+                Status = DidStatus.Pending,
+                CreatedAt = DateTime.UtcNow // Sempre use UTC
+            };
+
+            db.DidActivation.Add(didActivation);
+            await db.SaveChangesAsync();
+
+            return TypedResults.Created($"/api/DidActivation/{didActivation.Id}", didActivation);
+        })
+        .WithName("RequestDidActivation")
+        .WithSummary("Inicia uma nova ativação de DID apenas com o número.");
+
+        group.MapPatch("/{id}/status", async Task<Results<Ok<DidActivation>, NotFound>> (int id, DidStatusUpdateDto updateDto, AppDbContext db) =>
+        {
+            var didActivation = await db.DidActivation
+                .FirstOrDefaultAsync(model => model.Id == id);
+
+            if (didActivation is null)
+            {
+                return TypedResults.NotFound();
+            }
+
+            didActivation.Status = updateDto.Status;
+            didActivation.ErrorMessage = updateDto.ErrorMessage;
+            didActivation.UpdatedAt = DateTime.UtcNow;
+
+            await db.SaveChangesAsync();
+
+            return TypedResults.Ok(didActivation);
+        })
+        .WithName("UpdateDidActivationStatus")
+        .WithSummary("Atualiza o status e mensagem de erro de uma ativação de DID.");
 
         group.MapGet("/", async (AppDbContext db) =>
         {
