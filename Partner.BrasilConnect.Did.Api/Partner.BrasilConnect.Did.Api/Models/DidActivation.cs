30008aee85b18c2434742200cad46ede12b5570e
Paiva <danpaiva@live.com>
2025-11-15 11:02:52 -0300

    feat: add classe de modelo

---
+++ Partner.BrasilConnect.Did.Api/Partner.BrasilConnect.Did.Api/Models/DidActivation.cs
@@ -0,0 +1,17 @@
+﻿using Partner.BrasilConnect.Did.Api.Enum;
+using System.ComponentModel.DataAnnotations;
+using System.Net.NetworkInformation;
+
+namespace Partner.BrasilConnect.Did.Api.Models;
+
+public class DidActivation
+{
+    [Key]
+    public int Id { get; set; }
+    [Required(ErrorMessage = "Campo DidNumber obrigatorio.")]
+    public string DidNumber { get; set; }
+    public DidStatus Status { get; set; }
+    public string? ErrorMessage { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+}
